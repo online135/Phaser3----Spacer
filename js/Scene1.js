89bebdafdfa398dd8ba58388a0f3a647bd0e5fa5
online135 <b97b01067@ntu.edu.tw>
2023-02-23 00:31:46 +0800

    click picture and add point

--- js/Scene1.js
+++ js/Scene1.js
@@ -1,22 +1,41 @@
 class Scene1 extends Phaser.Scene {
   constructor() {
-    super("bootGame");
+    super("scene1");
+    this.score = 0;
+    this.spacePressed = false;
+  }
+
+  preload() {
+    this.load.image('picture', 'assets/paper.png');
   }
 
   create() {
 
-    const myText = this.add.text(40, 40, "bootGame...");
-    myText.setInteractive();
-    // this.scene.start("playGame");
+    this.add.image(400, 300, 'picture').setInteractive();
 
-    // add an event listener for the 'pointerup' event on the text object
-    myText.on('pointerup', () => {
-      // change to another Scene by calling the Scene Manager's start method with the scene key as the argument
-      this.scene.start('playGame');
-    });
+    this.scoreText = this.add.text(10, 10, 'Score: 0', { fontSize: '32px', fill: '#000' });
+
+    this.input.on('gameobjectup', this.onClickPicture, this);
+  }
+
+  onClickPicture(pointer, gameObject) {
+    this.score += 1;
+    this.scoreText.setText('Score: ' + this.score);
   }
 
   // upload() {
   //   if ()
+  // }
+
+  // update() {
+  //   if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE).isDown && !this.spacePressed) {
+  //     this.spacePressed = true;
+  //     this.score += 1;
+  //     this.scoreText.setText('Score: ' + this.score);
+  //   }
+
+  //   if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE).isUp) {
+  //     this.spacePressed = false;
+  //   }
   // }
 }
